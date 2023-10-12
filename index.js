dbd16e4fda417b1b8eafb2eaf5ab30070faa9e4f
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-10-12 19:31:34 +0300

    done

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const example = [
-  ["A", "A", "A"],
-  ["A", "A", "A"],
-  ["A", "B", "A"]];
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["P", "O", "O", "O"],
+  ["O", "O", "O", "O"]
+];
 
 const whereIsWaldo = (matrix) => {
-  
+  for(let i = 0 ; i<matrix.length;i++){
+   if(matrix[i].filter((element, index, array)=> array.indexOf(element)===index).length > 1)
+   {
+      const tempArray= Array.from(matrix[i]);
+      tempArray.sort();
+      console.log(tempArray)
+      if(tempArray[0]!==tempArray[1]){
+        return [i+1,matrix[i].indexOf(tempArray[0])+1];
+      }
+      else{
+        return [i+1,matrix[i].indexOf(tempArray[tempArray.length-1])+1];
+      }
+   }
+  }
 }
+
+console.log(whereIsWaldo(example));
